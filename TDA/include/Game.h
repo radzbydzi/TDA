95426a47dddff736790cb7aff096e8149014786a
radzbydzi <rdkbyd@gmail.com>
2016-11-09 15:54:46 +0100

    OGL w wątku działa, ale okno i konekst muszą być "na sztywno" zmienną globalną. Trzeba to zmienić.

--- TDA/include/Game.h
+++ TDA/include/Game.h
@@ -1,16 +1,31 @@
 #ifndef GAME_H
 #define GAME_H
-
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <string>
+#include <exception>
+#include <iostream>
 
 class Game
 {
     public:
         Game();
         virtual ~Game();
-
+        SDL_Thread *logicThread;//obiekt w¹tku dla elementow logicznych gry
+        SDL_Thread *renderThread;//obiekt w¹tku dla renderu scen
+        bool isRunning = true;
     protected:
 
     private:
+        //SDL_Window *window; // klasa okna
+        //SDL_GLContext context;//klasa kontekstu renderujacego
+        void logicThreadFunction();//tutaj znajdowac sie maja sie znajdowac wszystkie zmiany polozenia obiektow itp.
+        void renderThreadFunction();//tutaj renderowane sa obiekty brane z odpowiednich list
+        void interactionThreadFunction();//obsluga klawiatury myszy itp.; wywolane w watku glownym; tworzy takze okno
+        bool windowCreated = false;//jesli okno
+
 };
 
 #endif // GAME_H
--- TDA/src/Game.cpp
+++ TDA/src/Game.cpp
@@ -1,11 +1,246 @@
 #include "Game.h"
+SDL_mutex *lock;
+SDL_cond *cond = SDL_CreateCond();
+SDL_bool condition = SDL_FALSE;
+SDL_GLContext context;
+SDL_Window *window;
+void initGL() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClearDepth(1.0f);                   // Set background depth to farthest
+   glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+   glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+   glShadeModel(GL_SMOOTH);   // Enable smooth shading
+   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+}
+
+/* Handler for window-repaint event. Called back when the window first appears and
+   whenever the window needs to be re-painted. */
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+   glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+
+   // Render a color-cube consisting of 6 quads with different colors
+   glLoadIdentity();                 // Reset the model-view matrix
+   glTranslatef(1.5f, 0.0f, -7.0f);  // Move right and into the screen
+
+   glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+      // Top face (y = 1.0f)
+      // Define vertices in counter-clockwise (CCW) order with normal pointing out
+      glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f( 1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f,  1.0f);
+      glVertex3f( 1.0f, 1.0f,  1.0f);
+
+      // Bottom face (y = -1.0f)
+      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+      glVertex3f( 1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+
+      // Front face  (z = 1.0f)
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f( 1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f, -1.0f, 1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+      // Back face (z = -1.0f)
+      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f( 1.0f,  1.0f, -1.0f);
+
+      // Left face (x = -1.0f)
+      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(-1.0f,  1.0f,  1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+
+      // Right face (x = 1.0f)
+      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+      glVertex3f(1.0f,  1.0f, -1.0f);
+      glVertex3f(1.0f,  1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f, -1.0f);
+   glEnd();  // End of drawing color-cube
+
+   // Render a pyramid consists of 4 triangles
+   glLoadIdentity();                  // Reset the model-view matrix
+   glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
+
+   glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
+      // Front
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f(-1.0f, -1.0f, 1.0f);
+      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(1.0f, -1.0f, 1.0f);
+
+      // Right
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f(0.0f, 1.0f, 0.0f);
+      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(1.0f, -1.0f, 1.0f);
+      glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f(1.0f, -1.0f, -1.0f);
+
+      // Back
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f(0.0f, 1.0f, 0.0f);
+      glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f(1.0f, -1.0f, -1.0f);
+      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+
+      // Left
+      glColor3f(1.0f,0.0f,0.0f);       // Red
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glColor3f(0.0f,0.0f,1.0f);       // Blue
+      glVertex3f(-1.0f,-1.0f,-1.0f);
+      glColor3f(0.0f,1.0f,0.0f);       // Green
+      glVertex3f(-1.0f,-1.0f, 1.0f);
+   glEnd();   // Done drawing the pyramid
+
+}
+
+/* Handler for window re-size event. Called back when the window first appears and
+   whenever the window is re-sized with its new width and height */
+void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+   // Compute aspect ratio of the new window
+   if (height == 0) height = 1;                // To prevent divide by 0
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+   // Set the viewport to cover the new window
+   glViewport(0, 0, width, height);
+
+   // Set the aspect ratio of the clipping volume to match the viewport
+   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+   glLoadIdentity();             // Reset
+   // Enable perspective projection with fovy, aspect, zNear and zFar
+   gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
 
 Game::Game()
 {
     //ctor
+    //odpalamy watki
+
+    //logicThread = SDL_CreateThread((SDL_ThreadFunction)&logicThreadFunction, "logicThread", (void *)NULL);
+        renderThread = SDL_CreateThread((SDL_ThreadFunction)&renderThreadFunction, "renderThread", (void *)NULL);
+        interactionThreadFunction();
+
 }
 
 Game::~Game()
 {
     //dtor
+
+	//Destroy window
+	//SDL_DestroyWindow( window );
+
+	//Quit SDL subsystems
+	//SDL_Quit();
 }
+
+void Game::logicThreadFunction()
+{
+    printf("Dzialam \r\n");
+    while(isRunning)
+    {
+
+        //SDL_WaitThread(renderThread, NULL);
+    }
+}
+
+void Game::renderThreadFunction()
+{
+    printf("Mów do mnie! \r\n");
+
+    SDL_LockMutex(lock);            //czeka az utworzy sie okno
+    while (!condition) {            //
+        SDL_CondWait(cond, lock);   //
+    }                               //
+    SDL_UnlockMutex(lock);          //
+
+    //context = SDL_GL_CreateContext(window);
+    SDL_GLContext mainGlContext = SDL_GL_CreateContext(window);
+    //mainGlContext = SDL_GL_CreateContext(window);
+    //std::cout<<&context<<" "<<&mainGlContext<<std::endl;
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    // Double Buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_MakeCurrent(window, mainGlContext);
+    SDL_GL_SwapWindow(window);
+
+
+    while(isRunning)
+    {
+        glClearColor(0.0, 0.0, 0.0, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        reshape(640,480);
+        initGL();
+        display();
+        SDL_GL_SwapWindow(window);
+    }
+}
+
+void Game::interactionThreadFunction()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Event e;
+            // Initialize SDL2
+    //context = SDL_GL_CreateContext(window);
+    //SDL_GL_SetAttribute(SDL_GL_SHARE_WITH_CURRENT_CONTEXT, 1);
+    //SDL_GL_MakeCurrent(window, NULL);
+
+    //SDL_GL_SetSwapInterval(1);
+    // Check that the window was successfully created
+    window = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
+                                                             SDL_WINDOWPOS_UNDEFINED,
+                                                             640,
+                                                             480,
+                                                             SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+
+    }
+    SDL_LockMutex(lock);    //blokuje mutex
+    condition = SDL_TRUE;   //gdy sie to wykona
+    SDL_CondSignal(cond);   //odblokowuje
+    SDL_UnlockMutex(lock);  //wtedy render moze dzialac
+
+    while( isRunning )
+    {
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+
+            //User requests quit
+            if( e.type == SDL_QUIT )
+            {
+                isRunning = false;
+            }
+
+        }
+        //reshape(640,480);
+        //initGL();
+        //display();
+        //Swap back and front buffer
+        //SDL_GL_SwapWindow(window);
+        //SDL_GL_MakeCurrent(window, context);
+
+        //if(SDL_GL_GetCurrentContext())
+           // printf("Context > %s \r\n", SDL_GL_GetCurrentContext());
+    }
+}
+
+
